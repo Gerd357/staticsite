768c5d1dcc37479a2523951cdaf03275462e3cc0
Gerd357 <gerdgeco@gmail.com>
2025-07-12 04:10:13 +0200

    added inline markdown funcionality

--- src/functions.py
+++ src/functions.py
@@ -15,3 +15,25 @@ def text_node_to_html_node(text_node):
     if text_node.text_type == TextType.IMAGE:
         return LeafNode("img", "", {"src": f'{text_node.url}', "alt": f'{text_node.text}'})
     raise Exception("not from acceptable types")
+
+
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    result = []
+    for node in old_nodes:
+        if node.text_type is not TextType.TEXT:
+            result.append(node)
+        else:
+            split_list = node.text.split(delimiter)
+            if len(split_list) % 2 == 0:
+                raise Exception("Invalid markdown syntax")
+
+            for i, part in enumerate(split_list):
+                if split_list[i] == "":
+                    continue
+                if i % 2 == 0:  # Even index = regular text
+                    result.append(TextNode(part, TextType.TEXT))
+                else:  # Odd index = delimited text
+                    result.append(TextNode(part, text_type))
+    return result
