393620ec49c3b6489cf52501155862fc2b43249c
Gerd357 <gerdgeco@gmail.com>
2025-07-13 06:44:21 +0200

    finished the blocks section

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,6 @@
-from htmlnode import LeafNode
+from htmlnode import LeafNode , HTMLNode, ParentNode
 from textnode import TextType, TextNode
+from markdown_blocks import block_to_block_type, BlockType, markdown_to_blocks
 import re 
 
 def text_node_to_html_node(text_node):
@@ -108,14 +109,87 @@ def text_to_textnodes(text):
     return fifth
 
 
-def markdown_to_blocks(markdown):
-    new_list = []
-    text = markdown.strip()
-    split_text = text.split("\n\n")
+def text_to_children(text):
+    text_node_list = text_to_textnodes(text)
+    html_node_list = []
+    for text_node in text_node_list:
+        html_node_list.append(text_node_to_html_node(text_node))
+    return html_node_list
 
-    for item in split_text:
-        item = item.strip()
+def markdown_to_html_node(markdown):
+    block_list = markdown_to_blocks(markdown)
+    html_blocks = []
+    
+    for block in block_list:
+        block_type = block_to_block_type(block)
         
-        if item != "":
-            new_list.append(item)
-    return new_list
+        if block_type != BlockType.CODE:
+            if block_type == BlockType.PARAGRAPH:
+                block_tag = "p"
+                list_children = text_to_children(block) 
+                block_html = HTMLNode(block_tag, None, list_children)
+
+            if block_type == BlockType.HEADING:
+                block_tag = "h" + str(len(block) - len(block.lstrip("#"))) 
+                block_text = block.lstrip("#").lstrip()
+                list_children = text_to_children(block_text)
+                block_html = HTMLNode(block_tag, None, list_children)
+
+            if block_type == BlockType.QUOTE:
+                block_tag = "blockquote"
+                complete = []
+                split_quote = block.split("\n")
+                
+                for item in split_quote:
+                    complete.append(item.lstrip("> "))
+                block_text = "\n".join(complete)
+
+                list_children = text_to_children(block_text)
+                block_html = HTMLNode(block_tag, None, list_children)
+
+
+            if block_type == BlockType.ULIST:
+                block_tag = "ul"
+                split_block = block.split("\n")
+                list_nodes = []
+                html_element_list = []
+
+                for item in split_block:
+                    if item.startswith("*"):
+                        list_nodes.append(item.lstrip("* "))
+                    if item.startswith("-"):
+                        list_nodes.append(item.lstrip("- "))
+                    if item.startswith("+"):
+                        list_nodes.append(item.lstrip("+ "))
+
+                for item in list_nodes:
+                    list_children = text_to_children(item)
+                    html_element_list.append(HTMLNode("li", None, list_children))
+
+                block_html = HTMLNode("ul", None, html_element_list)
+                
+            if block_type == BlockType.OLIST:
+                block_tag = "ol"
+                split_block = block.split("\n")
+                list_nodes = []
+                html_element_list = []
+                counter = 1
+                for item in split_block:
+                    if item.startswith(str(counter) + "."):
+                        list_nodes.append(item.lstrip(str(counter) + ". "))
+                for item in list_nodes:
+                    list_children = text_to_children(item)
+                    html_element_list.append(HTMLNode("li", None, list_children)) 
+                
+                block_html = HTMLNode("ol", None, html_element_list)
+        else:
+            block_tag = "pre"
+            code_tag = "code"
+            block_text = block.strip("```")
+            text_node = TextNode(block_text, TextType.CODE)
+            code_html = text_node_to_html_node(text_node)
+            block_html = HTMLNode(block_tag, None, [code_html])
+
+        html_blocks.append(block_html)
+    
+    return HTMLNode("div", None, html_blocks)
